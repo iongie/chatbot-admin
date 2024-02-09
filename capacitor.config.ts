9f5d20be21067ba17a5215b105c5e3a76c2d3f30
Gigih Santoso <gigih985042@gmail.com>
2024-02-09 23:03:09 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chatbot-admin',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
